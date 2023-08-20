da0e3cc2bcb30fdc8369c3a7c4a2f5c1eb771390
Dery Galeas <dery.reyna@gmail.com>
2023-08-20 14:13:59 -0600

    update: dynamic routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,69 @@ import {api} from "~/utils/api";
 import type {RouterOutputs} from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
+import { useContext, useState } from "react";
+import toast from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime)
 
 const CreatePostWizard = ()=>{
+
   const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+  const {mutate, isLoading: isPosting} = api.posts.create.useMutation({
+    onSuccess: ()=>{
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+    onError : (e)=>{
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]){
+        toast.error(errorMessage[0]);
+      } else{
+        toast.error("Failed to post! Please try later")
+      }
+      
+    }
+  });
+
   console.log(user)
   if (!user) return null
 
   return(
     <div className="flex gap-3 w-full">
       <img src={user.imageUrl} alt="Profile Image" className="h-14 w-14 rounded-full" />
-      <input placeholder="Type Emojis!" className="bg-transparent grow outline-none"/>
+      <input 
+        disabled={isPosting} 
+        onChange={(e) => setInput(e.target.value)} 
+        type="text" value={input} placeholder="Type Emojis!" 
+        className="bg-transparent grow outline-none"
+        onKeyDown={(e)=>{
+          if (e.key === "Enter"){
+            e.preventDefault();
+            if (input !== ""){
+              mutate({content:input})
+            }
+          }
+        }} 
+      />
+      {input !=="" && !isPosting &&(
+        <button onClick={() => mutate({content:input})}>Post</button>
+      )}
+
+      {isPosting && 
+      <div className="flex items-center justify-center">
+        <LoadingSpinner size={20}/>  
+      </div>}
     </div>
   )
 }
 
-type PostWithUser = RouterOutputs["post"]["getAll"][number];
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 
 const PostView = (props: PostWithUser) => {
   const {post, author} = props;
@@ -29,26 +75,49 @@ const PostView = (props: PostWithUser) => {
       <img src={author.profileImageUrl} alt="Profile Image" className="h-14 w-14 rounded-full"/>
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username} `}</span>
-          <span className="font-thin">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username} `}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
+          
 
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
     
   )
 }
 
+const Feed = () =>{
+  const {data, isLoading: postLoading} = api.posts.getAll.useQuery();
+
+  if (postLoading) return <LoadingPage/>
+  
+  if (!data) return <div>Something went wrong!</div>
+
+  return(
+      <div className="flex flex-col">
+        {data?.map((fullPost)=>(
+          <PostView {...fullPost} key={fullPost.post.id}/>
+        
+        ))}
+      </div>
+
+  )
+}
+
 export default function Home() {
 
-  const user = useUser();
-  const {data, isLoading} = api.post.getAll.useQuery();
+  const {isLoaded: userLoaded, isSignedIn} = useUser();
 
-  if (isLoading) return <div>Loading...</div>
+  // Start fetching asap
+  api.posts.getAll.useQuery();
 
-  if(!data) return <div>Something went wrong!</div>
-
+  // Return empty div if both aren't loaded, since user tends to load faster
+  if (!userLoaded) return <div/>
 
   
   return (
@@ -63,20 +132,17 @@ export default function Home() {
 
         
           <div className="border-b border-slate-400 p-4 flex ">
-            {!user.isSignedIn && (
+            {!isSignedIn && (
               <div className="flex justify-center">
                 <SignInButton />  
               </div>
             )}
-            {user.isSignedIn && <CreatePostWizard/>}
+            {isSignedIn && <CreatePostWizard/>}
           </div>
 
-          <div className="flex flex-col">
-            {data?.map((fullPost)=>(
-              <PostView {...fullPost} key={fullPost.post.id}/>
-            
-            ))}
-          </div>
+          <Feed/>
+
+
         
         </div>
       </main>
---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,78 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import type { User } from '@clerk/nextjs/server';
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+
+
+const filterUserForClient = (user:User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl
+  }
+}
+
+
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "3 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */ 
+  prefix: "@upstash/ratelimit",
+});
+
+export const postRouter = createTRPCRouter({
+    
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take:100,
+      orderBy: [{createdAt: "desc"}]
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+    })
+    ).map(filterUserForClient)
+    console.log(users);
+
+    return posts.map((post)=>{
+      const author = users.find((user) => user.id === post.authorId)
+      if (!author || !author.username) throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not found"
+      })
+      return{
+      post,
+      author,
+    }})
+  }),
+
+  create: privateProcedure.input(z.object({
+    content: z.string().min(1).max(280)
+  })).mutation(async ({ctx, input}) =>{
+    const authorId = ctx.userId;
+
+   const {success} = await ratelimit.limit(authorId);
+
+   if (!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"})
+
+    const post = await ctx.prisma.post.create({
+      data: {
+        authorId,
+        content:input.content,
+      },
+    })
+    
+    return post
+  })
+});
